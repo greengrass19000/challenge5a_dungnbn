9a8ccda6d13d587bddc8e9e7df2de073a0f884e6
greengrass19000 <greengrass19000@gmail.com>
2022-10-14 21:53:33 +0700

    init

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Quản lý thông tin sinh viên</title>
+        <link rel="stylesheet" href="./styles.css">
+    </head>
+    <body>
+        <?php
+//        echo "Hello"
+        ?>
+        <form action="login.php" method="post">
+            Tài khoản : <input type="text" name="Tài khoản" id="u" space-holder="typehere">
+            Mật khẩu : <input type="text" name="Mật khẩu" id="p" space-holder="typehere">
+            <input type="submit" value="Đăng nhập">
+        </form>
+    </body>
+</html>
---
+++ user-info.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Info</title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+        nothing
+    </body>
+</html>
